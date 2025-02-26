9f9be7b2933b2060762b24e9da422eeed41f35a5
iara <iaraotako@gmail.com>
2025-02-25 21:41:40 -0300

    25_02

---
+++ 19_02/tabela_pizzaria.sql
@@ -0,0 +1,52 @@
+
+use pizzaria;
+create table pizza(
+id_pizza int auto_increment primary key,
+sabor varchar(250),
+ingredientes varchar(250)
+);
+
+alter table pizza 
+add preco dec(6,2);
+
+create user 'luiz-maia'@'%' identified by 'senai@123';
+grant insert,update,select on pizzaria.pizza to 'luiz-maia'@'%';
+grant delete on pizzaria.pizza to 'luiz-maia'@'%';
+grant select on pizzaria.au_pizza to 'luiz-maia'@'%';
+
+DELIMITER $$
+create trigger depois_insert_pizza
+after insert on pizza
+for each row
+begin
+insert into au_pizza(id_pizza, sabor, ingredientes, usuario, data_hora, operacao)
+values (new.id_pizza, new.sabor, new.ingredientes, user(), now(), 'insert');
+end$$
+
+DELIMITER ;
+
+
+DELIMITER $$
+create trigger depois_update_pizza
+after update on pizza
+for each row
+begin
+insert into au_pizza(id_pizza, sabor, ingredientes, usuario, data_hora, operacao)
+values (old.id_pizza, old.sabor, old.ingredientes, user(), now(), 'update');
+end$$
+
+DELIMITER ;
+
+DELIMITER $$
+create trigger depois_delete_pizza
+before delete on pizza
+for each row
+begin
+insert into au_pizza(id_pizza, sabor, ingredientes, usuario, data_hora, operacao)
+values (old.id_pizza, old.sabor, old.ingredientes, user(), now(), 'delete');
+end$$
+
+DELIMITER ;
+
+insert into pizza (sabor, ingredientes) values('banana', 'canela e banana');
+
